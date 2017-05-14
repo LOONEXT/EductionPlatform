e5c31b6113faad1c7a10b90ab09394b5d676ed36
shineit-kefei <kefei@openerp.cn>
2017-05-14 17:27:14 +0800

    创建邮箱验证码、轮播图模型

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.utils.translation import ugettext as _
@@ -22,4 +23,30 @@ class UserProfile(AbstractUser): # AbstractUser 作为一个抽象基类`abstrac
 
     class Meta:
         verbose_name = _('User Message')
+        verbose_name_plural = verbose_name
+
+
+class EmailVerifyRecord(models.Model):
+    """邮箱注册、找回密码生成邮箱验证码"""
+    code = models.CharField(max_length=20, verbose_name=_('Verification Code'))
+    email = models.EmailField(max_length=50, verbose_name=_('Email'))
+    send_type = models.CharField(max_length=10, choices=(('register', _('Register')), ('forget', _('Forget Password'))))
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = _('Email Verification Code')
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    """首页轮播图数据库模型"""
+    title = models.CharField(max_length=100, verbose_name=_('title'))
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=_('Image'))
+    url = models.URLField(max_length=200, verbose_name=_('Enter URL'))
+    # 轮播图位置顺序
+    index = models.IntegerField(default=100, verbose_name=_('order'))
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=_('Add Time'))
+
+    class Meta:
+        verbose_name = _('Banner')
         verbose_name_plural = verbose_name
